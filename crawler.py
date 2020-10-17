29fd43ce3ff75523703cb165511441399b4ad75a
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2020-10-17 09:16:22 +0330

    Bug fixed

--- crawler.py
+++ crawler.py
@@ -196,7 +196,7 @@ def save_web_page_in_db(web_page):
         'url': web_page.url,
         'out_degree': web_page.out_degree,
         'size': web_page.size,
-        'compressed_size': web_page.size,
+        'compressed_size': web_page.compressed_size,
         'status_code': web_page.status_code,
         'crawled': web_page.crawled,
         'compressed_content': web_page.compressed_content,
